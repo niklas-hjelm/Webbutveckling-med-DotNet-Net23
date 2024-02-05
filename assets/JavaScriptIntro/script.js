287beea73a33b0d6b0f63044bc6e192cb8e4f7ca
niklas-hjelm <niklas.hjelm@iths.se>
2024-02-05 15:33:32 +0100

    Update övningar

--- assets/JavaScriptIntro/script.js
+++ assets/JavaScriptIntro/script.js
@@ -90,3 +90,69 @@ console.log("-------------");
 let splice = arr3.splice(2, 1, 10);
 
 console.log(splice, arr3);
+
+//Upopgift 1
+function filterNumbers(arr) {
+  return arr.filter((n) => n > 10);
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const filteredNumbers = filterNumbers(numbers);
+
+console.log(filteredNumbers);
+
+//Uppgift 2
+const book = {
+  titel: "Hej",
+  författare: "Niklas",
+  utgivningsdatum: 9001,
+};
+
+function printBok(b) {
+  console.log(`${b.titel} av ${b.författare} utgiven år ${b.utgivningsdatum}`);
+}
+
+printBok(book);
+
+//Uppgift 3
+function isEven(tal) {
+  //   if (tal % 2 === 0) {
+  //     console.log("Talet är jämt!");
+  //   } else {
+  //     console.log("Talet är udda!");
+  //   }
+  tal % 2 == 0 ? console.log("Talet är jämt!") : console.log("Talet är udda!");
+}
+
+isEven(1);
+isEven(2);
+
+//Uppgift 4
+// Fibbonaccis talsekvens består av en följd av tal
+// där varje tal är summan av de föregående två talen.
+// 0 1 1 2 3 5 8 13 21 34
+
+const fibbo = [0, 1];
+
+function getFibboNums(fibboArr, stopCount) {
+  if (fibboArr.length == stopCount) {
+    return;
+  }
+  fibboArr.push(fibboArr[fibboArr.length - 1] + fibboArr[fibboArr.length - 2]);
+  getFibboNums(fibboArr, stopCount);
+}
+
+getFibboNums(fibbo, 10);
+
+console.log(fibbo);
+
+// Uppgift 5
+const strings = ["hej", "på", "dig"];
+
+function allStringsToUpper(arr) {
+  return arr.map((s) => s.toUpperCase());
+}
+
+const result = allStringsToUpper(strings);
+
+console.log(result);
